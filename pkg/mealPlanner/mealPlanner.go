a91e67eab28305374ea05133aed844b45ce2f33b
Felix <felixrosat@gmail.com>
2022-10-16 09:31:48 +0200

    Query database

--- pkg/mealPlanner/mealPlanner.go
+++ pkg/mealPlanner/mealPlanner.go
@@ -1,11 +1,29 @@
 package mealPlanner
 
 import (
+	"context"
 	"fmt"
 	"os"
+
+	"github.com/jomei/notionapi"
 )
 
 func Plan() {
-	notionToken := os.Getenv("NOTION_TOKEN")
-	fmt.Fprintf(os.Stdout, "'%s'", notionToken)
+	notionToken := notionapi.Token(os.Getenv("NOTION_TOKEN"))
+	fmt.Fprintf(os.Stdout, "'%s' \n", notionToken)
+
+	client := notionapi.NewClient(notionToken)
+
+	fmt.Println("Client Created")
+	recipesDbId := notionapi.DatabaseID(os.Getenv("RECIPES_DB_ID"))
+
+	query := new(notionapi.DatabaseQueryRequest)
+	recipesDb, err := client.Database.Query(context.Background(), recipesDbId, query)
+	if err != nil {
+		fmt.Println("Error while getting db")
+
+		// do something
+	}
+
+	fmt.Printf("%+v\n", recipesDb.Results)
 }
