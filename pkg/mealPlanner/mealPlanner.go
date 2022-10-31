ed4c9e9e9576db286137528573b67e3110081ed4
Felix <felixrosat@gmail.com>
2022-10-31 17:03:55 +0100

    Introduce flag to enter tags

--- pkg/mealPlanner/mealPlanner.go
+++ pkg/mealPlanner/mealPlanner.go
@@ -9,7 +9,7 @@ import (
 	"github.com/rs/zerolog/log"
 )
 
-func Plan() {
+func Plan(tags []string) {
 	notionToken := notionapi.Token(os.Getenv("NOTION_TOKEN"))
 	log.Debug().Msgf("'%s' \n", notionToken)
 
@@ -18,16 +18,12 @@ func Plan() {
 	log.Debug().Msg("Client Created")
 	fmt.Println()
 
-	getRecipes(*client)
-}
-
-func ListRecipes() {
-	notionToken := notionapi.Token(os.Getenv("NOTION_TOKEN"))
-	client := notionapi.NewClient(notionToken)
-
-	log.Debug().Msg("Client Created")
-
-	recipes := getRecipes(*client)
+	var recipes []notionapi.Page
+	if len(tags) != 0 {
+		recipes = getRecipes(*client, tags[0])
+	} else {
+		recipes = getRecipes(*client, "")
+	}
 
 	for index, recipe := range recipes {
 		log.Debug().Msgf("%+v\n", recipe)
@@ -41,10 +37,42 @@ func ListRecipes() {
 	}
 }
 
-func getRecipes(client notionapi.Client) []notionapi.Page {
+func ListRecipes(tags []string) {
+	notionToken := notionapi.Token(os.Getenv("NOTION_TOKEN"))
+	client := notionapi.NewClient(notionToken)
+
+	log.Debug().Msg("Client Created")
+
+	var recipes []notionapi.Page
+	if len(tags) != 0 {
+		recipes = getRecipes(*client, tags[0])
+	} else {
+		recipes = getRecipes(*client, "")
+	}
+
+	for index, recipe := range recipes {
+		log.Debug().Msgf("%+v\n", recipe)
+		if recipeNameProperty, ok := recipe.Properties["Name"].(*notionapi.TitleProperty); ok {
+			recipeName := recipeNameProperty.Title[0].PlainText
+			fmt.Printf("%d. %s \n", index+1, recipeName)
+		} else {
+			log.Error().Msg("There was an error getting the recipe name")
+		}
+
+	}
+}
+
+func getRecipes(client notionapi.Client, tag string) []notionapi.Page {
 	recipesDbId := notionapi.DatabaseID(os.Getenv("RECIPES_DB_ID"))
 
 	query := new(notionapi.DatabaseQueryRequest)
+	if tag != "" {
+		filter := new(notionapi.PropertyFilter)
+		filter.Property = "Tags"
+		filter.MultiSelect = new(notionapi.MultiSelectFilterCondition)
+		filter.MultiSelect.Contains = tag
+		query.Filter = filter
+	}
 	recipesDb, err := client.Database.Query(context.Background(), recipesDbId, query)
 	if err != nil {
 		log.Error().Msg("Error while getting db")
